120a0b60b551fefcd44a2d4a8a9908f5d245b99b
Mashhurakhon <140955020+Mashhurakhon@users.noreply.github.com>
2024-07-16 12:17:55 +0500

    исправленные данные и код

--- src/app/components/input-state/input-state.component.ts
+++ src/app/components/input-state/input-state.component.ts
@@ -1,71 +1,43 @@
-import { InputState } from '../../models/input-state';
-import { WordsService } from './../../services/words.service';
-import { Component, EventEmitter, OnInit, output, Output } from '@angular/core';
+import {Component} from '@angular/core';
+import {InputState} from "../../models/input-state";
+
 @Component({
   selector: 'app-input-state',
   templateUrl: './input-state.component.html',
-  styleUrl: './input-state.component.css'
+  styleUrls: ['./input-state.component.css']
 })
-export class InputStateComponent implements OnInit{
-  public inputContent!: string;
-  public isDisabledBtn: boolean = true;
-  public val: string = 'focus';
-  public datas!: InputState[];
-  public words: InputState = {
-    id: '',
-    str: ''
+export class InputStateComponent {
+  /** Текстовое содержимое поля ввода */
+  inputContent: string = '';
+
+  /** Состояние поля ввода:
+   * 'default','focus', 'warning', 'error', 'success'
+   * */
+  inputState: string = 'default';
+
+  onFocus(): void {
+    this.inputState = 'focus';
   }
 
-  @Output() exIsDisabled = new EventEmitter<boolean>();
-  sendResult() {
-    this.exIsDisabled.emit(this.isDisabledBtn);
-  }
-  constructor(private wordsService: WordsService) {}
-
-  ngOnInit() {
-    this.getWordsQuery();
-  }
-
-  getWordsQuery() {
-    this.wordsService.getWords()
-    .subscribe(words => {
-      this.datas = words;
-    });
+  /**
+   * Обработчик события потери фокуса полем ввода.
+   * Определяет состояние поля ввода в зависимости от его содержимого.
+   */
+  onBlur(): void {
+    if (this.inputContent === '') {
+      this.inputState = 'default';
+    } else if (this.inputContent.length < 3) {
+      this.inputState = 'warning';
+    } else {
+      this.inputState = 'success';
+    }
   }
 
-  postWordsQuery() {
-    this.words = {
-      id: '',
-      str: this.inputContent
-    }
-    this.wordsService.postWords(this.words)
-    .subscribe(word => this.words)
-  }
-  addData() {
-    // this.datas.push(this.inputContent);
-  }
-  errorData(){
-    this.val = 'warn';
-    return (this.inputContent.length <= 3); 
-  }
-  nums: string = '1234567890'
-  warnData() {
-    this.val = 'error';
-    return (this.nums.includes(this.inputContent[0]));
-  }
-  inputData() {
-    if(!this.warnData()) {
-      this.val = 'warn';
-      this.isDisabledBtn = true;
-    }
-    if(this.errorData()){
-      this.val = 'error';
-      this.isDisabledBtn = true;
-    }
-    if (!this.warnData() && !this.errorData()) {
-      this.val = 'success';
-      this.isDisabledBtn = false;
-    }
+  /**
+   * Возвращает текущее состояние поля ввода для установки класса стилей.
+   * @returns {string} Текущее состояние поля ввода ('default', 'focus', 'warning', 'error', 'success').
+   */
+  getInputClass(): string {
+    return this.inputState;
   }
 }
-
